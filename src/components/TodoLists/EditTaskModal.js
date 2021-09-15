c034c837a0bc80c4a299c4b45faa0d9e7a5356d8
Shweta <gajakoshshweta30@gmail.com>
2021-09-15 15:06:00 +0530

    todo list

--- src/components/TodoLists/EditTaskModal.js
+++ src/components/TodoLists/EditTaskModal.js
@@ -1,58 +1,59 @@
 import React, { useEffect, useState } from "react";
 
 export const EditTaskModal = ({ showModal, task, onUpdate, onCloseModal }) => {
-    console.log("task", task);
-    const [updatedTask, setTask] = useState(task || '');
+  console.log("task", task);
+  const [updatedTask, setTask] = useState(task || "");
 
-    useEffect(() => {
-        setTask(task || '');
-    }, [task]);
+  useEffect(() => {
+    setTask(task || "");
+  }, [task]);
 
-    const handleOnChange = (e) => {
-        setTask(e.target.value || '');
-    };
+  const handleOnChange = (e) => {
+    setTask(e.target.value || "");
+  };
 
-    return (
-        <div
-            className={showModal ? "modal fade show" : "modal fade"}
-            tabIndex="-1"
-            style={{display: showModal ? 'block' : 'none'}}
-        >
-            <div className="modal-dialog">
-                <div className="modal-content">
-                    <div className="modal-header">
-                        <h5 className="modal-title" id="exampleModalLabel">
-                            Edit task
-                        </h5>
-                        <button
-                            type="button"
-                            className="btn-close"
-                        ></button>
-                    </div>
-                    <div className="modal-body">
-                        <input
-                            type="text"
-                            className="form-control"
-                            id="todo-task"
-                            onChange={handleOnChange}
-                            value={updatedTask}
-                            placeholder="New Task"
-                        />
-                    </div>
-                    <div className="modal-footer">
-                        <button
-                            type="button"
-                            className="btn btn-secondary"
-                            onClick={onCloseModal}
-                        >
-                            Close
-                        </button>
-                        <button type="button" className="btn btn-primary" onClick={() => onUpdate(updatedTask)}>
-                            Save changes
-                        </button>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div
+      className={showModal ? "modal fade show" : "modal fade"}
+      tabIndex="-1"
+      style={{ display: showModal ? "block" : "none" }}
+    >
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title" id="exampleModalLabel">
+              Edit task
+            </h5>
+            <button type="button" className="btn-close"></button>
+          </div>
+          <div className="modal-body">
+            <input
+              type="text"
+              className="form-control"
+              id="todo-task"
+              onChange={handleOnChange}
+              value={updatedTask}
+              placeholder="New Task"
+            />
+          </div>
+          <div className="modal-footer">
+            <button
+              type="button"
+              className="btn btn-secondary"
+              onClick={onCloseModal}
+            >
+              Close
+            </button>
+            <button
+              type="button"
+              className="btn btn-primary"
+              onClick={() => onUpdate(updatedTask)}
+            >
+              Save changes
+            </button>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
--- src/components/TodoLists/NewTodoTask.js
+++ src/components/TodoLists/NewTodoTask.js
@@ -2,46 +2,45 @@ import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 export const NewTodoTask = ({ addTodoTask }) => {
-    const [task, setTask] = useState("");
-  
+  const [task, setTask] = useState("");
 
-    const handleOnChange = (e) => {
-        setTask(e.target.value);
-    };
+  const handleOnChange = (e) => {
+    setTask(e.target.value);
+  };
 
-    const addNewTask = () => {
-        addTodoTask(task);
-        setTask("");
-    };
+  const addNewTask = () => {
+    addTodoTask(task);
+    setTask("");
+  };
 
-    return (
-        <>
-            <form className="row g-3" >
-                <div className="col-sm-9" style={{paddingLeft:"20%"}}> 
-                    <input
-                        type="text"
-                        className="form-control"
-                        id="todo-task"
-                        onChange={handleOnChange}
-                        value={task}
-                        placeholder="New Task"
-                    />
-                </div>
-                <div className="col-sm-3">
-                    <button
-                        type="button"
-                        className="btn btn-primary"
-                        onClick={addNewTask}
-                        disabled={!task}
-                    >
-                        Add 
-                    </button>
-                </div>
-            </form>
-        </>
-    );
+  return (
+    <>
+      <form className="row g-3">
+        <div className="col-sm-9" style={{ paddingLeft: "20%" }}>
+          <input
+            type="text"
+            className="form-control"
+            id="todo-task"
+            onChange={handleOnChange}
+            value={task}
+            placeholder="New Task"
+          />
+        </div>
+        <div className="col-sm-3">
+          <button
+            type="button"
+            className="btn btn-primary"
+            onClick={addNewTask}
+            disabled={!task}
+          >
+            Add
+          </button>
+        </div>
+      </form>
+    </>
+  );
 };
 
 NewTodoTask.propTypes = {
-    addTodoTask: PropTypes.func.isRequired,
+  addTodoTask: PropTypes.func.isRequired,
 };
